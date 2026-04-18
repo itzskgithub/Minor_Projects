690f4d11d24e664ebd3885318fb4f3a6ec6960fd
itzskgithub <shubhampandey8969@gmail.com>
2026-04-18 11:49:10 +0530

    Duplicates deleted

--- Async_projects/one.js
+++ Async_projects/one.js
@@ -1,28 +1,3 @@
-/*
-const randomcolor = function(){
-    let color = '#';
-    let hex = '0123456789ABCDEF';
-    for(let i = 0; i < 6; i++){
-        color += hex[Math.floor(Math.random()*16)];
-    }
-    return color;
-};
-
-function changecolor() {document.body.style.backgroundColor = randomcolor()};
-let id;
-document.querySelector('#start').addEventListener('click' , function(){
-    if(!id){
-        id = setInterval(changecolor, 50);
-    }
-});
-
-    document.querySelector('#stop').addEventListener('click' , function(){
-    clearInterval(id);
-    id = null;
-
-});
-*/
-
 let hex = "0123456789ABCDEF";
 
 function randomcolor() {
